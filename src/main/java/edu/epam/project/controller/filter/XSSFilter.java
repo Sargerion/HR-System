f819fa0f5c37e04a59c6844355ab8b380afe4874
Sargerion <56206480+Sargerion@users.noreply.github.com>
2021-02-21 06:27:18 +0300

    Just XSS filter add

--- src/main/java/edu/epam/project/controller/filter/XSSFilter.java
+++ src/main/java/edu/epam/project/controller/filter/XSSFilter.java
@@ -1,20 +1,24 @@
 package edu.epam.project.controller.filter;
 
 import javax.servlet.*;
-import javax.servlet.annotation.*;
+import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebFilter(urlPatterns = {"/*"})
 public class XSSFilter implements Filter {
 
-    public void init(FilterConfig config) throws ServletException {
-    }
-
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        Enumeration<String> allAttributes = request.getAttributeNames();
+        while (allAttributes.hasMoreElements()) {
+            String parameterName = allAttributes.nextElement();
+            String parameterValue = request.getParameter(parameterName);
+            if (parameterValue != null) {
+                String newParameterValue = parameterValue.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+                request.setAttribute(parameterName, newParameterValue);
+            }
+        }
         chain.doFilter(request, response);
-    }
-
-    public void destroy() {
     }
 }
